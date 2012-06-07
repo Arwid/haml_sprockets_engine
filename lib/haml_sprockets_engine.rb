099cd9ece34a49a4b1b7945ccec1fc3d72fbda31
Arwid Bancewicz <arwid@arwid.ca>
2012-06-07 13:39:28 -0400

    latest

--- lib/haml_sprockets_engine.rb
+++ lib/haml_sprockets_engine.rb
@@ -1,2 +1,84 @@
+require 'haml_sprockets_engine/version'
+require 'tilt'
+require 'sprockets'
+require 'haml'
+
 module HamlSprocketsEngine
+
+  # require 'sprockets'
+  # require 'tilt'
+
+  class HamlSprockets < Tilt::Template
+    require 'haml'
+
+    def self.engine_initialized?
+      defined? ::Haml::Engine
+    end
+
+    def self.default_mime_type
+      'application/javascript'
+    end    
+
+    def initialize_engine
+      require_template_library 'haml'
+    end
+
+    def prepare
+      @engine = ::Haml::Engine.new(data, options)
+    end
+
+    def evaluate(scope, locals, &block)
+      if @engine.respond_to?(:precompiled_method_return_value, true)
+        super
+      else
+        @engine.render(scope, locals, &block)
+      end
+    end
+
+    # Precompiled Haml source. Taken from the precompiled_with_ambles
+    # method in Haml::Precompiler:
+    # http://github.com/nex3/haml/blob/master/lib/haml/precompiler.rb#L111-126
+    def precompiled_template(locals)
+      @engine.precompiled
+    end
+
+    def precompiled_preamble(locals)
+      local_assigns = super
+      @engine.instance_eval do
+        <<-RUBY
+          begin
+            extend Haml::Helpers
+            _hamlout = @haml_buffer = Haml::Buffer.new(@haml_buffer, #{options_for_buffer.inspect})
+            _erbout = _hamlout.buffer
+            __in_erb_template = false #true
+            _haml_locals = locals
+            #{local_assigns}
+        RUBY
+      end
+    end
+
+    def precompiled_postamble(locals)
+      @engine.instance_eval do
+        <<-RUBY
+            #{precompiled_method_return_value}
+          ensure
+            @haml_buffer = @haml_buffer.upper
+          end
+        RUBY
+      end
+    end
+  end
 end
+
+Sprockets::Engines
+Sprockets.register_engine '.haml', HamlSprocketsEngine::HamlSprockets
+
+# if Rails
+#   Rails.application.assets.register_engine '.haml', HamlSprocketsEngine::HamlSprockets
+# else
+#   Sprockets.register_engine '.haml', HamlSprocketsEngine::HamlSprockets
+# end
+
+
+require 'haml_sprockets_engine/engine' if defined?(Rails)
+
---
+++ lib/haml_sprockets_engine/engine.rb
@@ -0,0 +1,7 @@
+module HamlSprocketsEngine
+  module HamlSprockets
+    class Engine < ::Rails::Engine
+      # just to get the vendor directories included into assets
+    end
+  end
+end
